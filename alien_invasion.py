6474bc286ed9244e7213483dde4c15423734a6ef
zmy <573093826@qq.com>
2018-06-06 11:22:09 +0800

    Initial commit

---
+++ alien_invasion.py
@@ -0,0 +1,12 @@
+import sys
+import pygame
+
+
+def run_game():
+    # 初始化游戏并创建一个屏幕对象
+    pygame.init()
+    screen = pygame.display.set_mode((1200, 800))
+    pygame.display.set_caption("Alien Invasion")
+
+
+run_game()
